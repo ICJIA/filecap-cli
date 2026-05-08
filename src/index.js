5d6e43af2533d263e6af7ca159b04e00eb0bc708
cschweda <cschweda@gmail.com>
2026-05-08 17:04:41 -0500

    feat: re-export docx/xlsx/office-legacy schemas + introspectors from package main

--- src/index.js
+++ src/index.js
@@ -4,9 +4,15 @@ export {
   entrySchema,
   footerSchema,
   pdfIntrospectionSchema,
+  docxIntrospectionSchema,
+  xlsxIntrospectionSchema,
+  legacyOfficeIntrospectionSchema,
   isCompleteInventory,
   SCHEMA_VERSION,
 } from "./schema/inventory.js";
 export { introspect } from "./introspect/index.js";
 export { introspectPdf } from "./introspect/pdf.js";
+export { introspectDocx } from "./introspect/docx.js";
+export { introspectXlsx } from "./introspect/xlsx.js";
+export { introspectLegacyOffice } from "./introspect/office-legacy.js";
 export { FILECAP_VERSION } from "./version.js";
