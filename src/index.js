ed499d612739e1d2306171a4a00dac26be63fc88
cschweda <cschweda@gmail.com>
2026-05-08 16:23:28 -0500

    feat: re-export pdfIntrospectionSchema, introspect, introspectPdf from package main

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ export {
   headerSchema,
   entrySchema,
   footerSchema,
+  pdfIntrospectionSchema,
   isCompleteInventory,
   SCHEMA_VERSION,
 } from "./schema/inventory.js";
+export { introspect } from "./introspect/index.js";
+export { introspectPdf } from "./introspect/pdf.js";
 export { FILECAP_VERSION } from "./version.js";
