b0341c52c01abcb0f752abb1907544ef447741b1
cschweda <cschweda@gmail.com>
2026-05-08 13:40:53 -0500

    style: prettier format src/version.js (collapse JSON.parse to one line)

--- src/version.js
+++ src/version.js
@@ -5,8 +5,6 @@ import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const pkg = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, "..", "package.json"), "utf8"),
-);
+const pkg = JSON.parse(fs.readFileSync(path.resolve(__dirname, "..", "package.json"), "utf8"));
 
 export const FILECAP_VERSION = pkg.version;
