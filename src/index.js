7856776bf41cf68c702b0473ede65927e45b1692
cschweda <cschweda@gmail.com>
2026-05-08 20:03:38 -0500

    feat: re-export report machinery from package main

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export { runScan } from "./commands/scan.js";
 export { runRollup } from "./commands/rollup.js";
+export { runReport } from "./commands/report.js";
 export {
   headerSchema,
   entrySchema,
@@ -22,4 +23,13 @@ export { introspectLegacyOffice } from "./introspect/office-legacy.js";
 export { computeFilenameFlags } from "./flag/filename.js";
 export { rollupInventories } from "./rollup/merge.js";
 export { pickCanonical } from "./rollup/canonical.js";
+export { writeCsv, CSV_COLUMNS } from "./report/csv.js";
+export { writeSummary } from "./report/summary.js";
+export {
+  writeLargestFiles,
+  writeFlaggedFilenames,
+  writeDuplicateHashes,
+  writePdfImageOnly,
+} from "./report/flagged.js";
+export { humanizeBytes, csvCell } from "./report/format.js";
 export { FILECAP_VERSION } from "./version.js";
